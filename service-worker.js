d902d9d5a75e9b7bcc85ff7dde966f9dd0fa6900
sanyanemesh <sanya.nemesh@gmail.comgit config --global user.name sanyanemeshgit config --global user.email sanya.nemesh@gmail.com>
2020-04-22 10:04:10 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3ea1ecaf21e88fb12048508e358e9251.js"
+  "./precache-manifest.29de92cd2dcfec90f84f8b1a3a6efaa4.js"
 );
 
 self.addEventListener('message', (event) => {
